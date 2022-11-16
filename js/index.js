ea92e8710c2fbe0b019a7738eb72bf8d99049fb2
PoolloverNathan <nathan.kulzer@protonmail.com>
2022-11-16 19:36:31 +0000

    fractional changes

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function checkIntroStage() {
     if (localStorage.getItem("inco?") == null) {
       if (introStage == 1) {
         document.addEventListener("DOMContentLoaded", function(e){
-    document.getElementById("introText").innerHTML = "Welcome to Midivle!";})
+        zdocument.getElementById("introText").innerHTML = "Welcome to Midivle!";})
       } else {
         if (introStage == 2) {
           document.getElementById("introText").innerHTML = "This is a game all about building you kingdom and ending others, building alliances and ending alliances, hiring and firing, and saving lives and ending them too.  ";
@@ -24,18 +24,20 @@ function checkIntroStage() {
 
 checkIntroStage()
 
+let add = ""
+
 function loading1() {
   if (localStorage.getItem("inco?") == "true") {
-    document.getElementById("introText").innerHTML = "Loading."
+    document.getElementById("introText").innerHTML = "Loading." + add
   }
 }
 
 function loading2() {
-  document.getElementById("introText").innerHTML = "Loading.."
+  document.getElementById("introText").innerHTML = "Loading.." + add
 }
 
 function loading3() {
-  document.getElementById("introText").innerHTML = "Loading..."
+  document.getElementById("introText").innerHTML = "Loading..." + add
 }
 
 if (localStorage.getItem("inco?") == "true") {
@@ -45,6 +47,7 @@ if (localStorage.getItem("inco?") == "true") {
   setTimeout(loading1, 3000);
   setTimeout(loading2, 4000);
   setTimeout(loading3, 5000);
+  setTimeout(() => add = "<br>If you see this for a while, please try refreshing the page."), 10000)
 }
 
 if (localStorage.getItem("inco?") == null) {
