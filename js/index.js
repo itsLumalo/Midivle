945607d897b97ec3022bca7b678c7b9eda29b292
Project LuLo <67765714+project-lulo@users.noreply.github.com>
2022-11-15 20:30:41 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,8 +2,7 @@ var introStage = 1
 var introComplete = localStorage.getItem("inco?")
 var loadingClock = 0
  
-
-onload = function checkIntroStage() {
+function checkIntroStage() {
   if (localStorage.getItem("inco?") == null) {
       if (introStage == 1) {
         document.getElementById("introText").innerHTML = "Welcome to Midivle!";
