4bf728554618c1c5fd7283ffddb4bfb2da9fa5bb
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2022-11-16 13:58:53 -0500

    Clean up code

--- js/index.js
+++ js/index.js
@@ -1,21 +1,20 @@
 var introStage = 1
-var introComplete = localStorage.getItem("inco?")
-var loadingClock = 0
-
-
+// unused: var introComplete = localStorage.getItem("inco?")
+// unused: var loadingClock = 0
 
 function checkIntroStage() {
-    if (localStorage.getItem("inco?") == null) {
+    if (!localStorage.inco) {
       if (introStage == 1) {
-        document.addEventListener("DOMContentLoaded", function(e){
-    document.getElementById("introText").innerHTML = "Welcome to Midivle!";})
+        document.addEventListener("DOMContentLoaded", e => {
+          document.getElementById("introText").innerHTML = "Welcome to Midivle!";
+        })
       } else {
         if (introStage == 2) {
           document.getElementById("introText").innerHTML = "This is a game all about building you kingdom and ending others, building alliances and ending alliances, hiring and firing, and saving lives and ending them too.  ";
         } else {
           if (introStage == 3) {
             document.getElementById("introText").innerHTML = "Have fun and dont forget, only the best kingdom will rise!"
-            localStorage.setItem("inco?", "true");
+            localStorage.inco = true;
           }
         }
       }
@@ -25,29 +24,29 @@ function checkIntroStage() {
 checkIntroStage()
 
 function loading1() {
-  if (localStorage.getItem("inco?") == "true") {
+  if (localStorage.inco === true) {
     document.getElementById("introText").innerHTML = "Loading."
   }
+  setTimeout(loading1, 3000)
 }
 
 function loading2() {
   document.getElementById("introText").innerHTML = "Loading.."
+  setTimeout(loading2, 3000)
 }
 
 function loading3() {
   document.getElementById("introText").innerHTML = "Loading..."
+  setTimeout(loading3, 3000)
 }
 
-if (localStorage.getItem("inco?") == "true") {
+if (localStorage.inco) {
   setTimeout(loading1, 0);
   setTimeout(loading2, 1000);
   setTimeout(loading3, 2000);
-  setTimeout(loading1, 3000);
-  setTimeout(loading2, 4000);
-  setTimeout(loading3, 5000);
 }
 
-if (localStorage.getItem("inco?") == null) {
+if (!localStorage.inco) {
   $('body').keyup(function(spacePressed) {
     if (spacePressed.keyCode == 32) {
       introStage = introStage + 1;
