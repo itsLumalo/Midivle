1a087804cbb84abee300b6383f3fbd20e856ab3c
Project LuLo <67765714+project-lulo@users.noreply.github.com>
2022-11-15 23:15:33 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,8 @@ var loadingClock = 0
 function checkIntroStage() {
     if (localStorage.getItem("inco?") == null) {
       if (introStage == 1) {
-        document.getElementById("introText").innerHTML = "Welcome to Midivle!";
+        document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("introText").innerHTML = "Welcome to Midivle!";})
       } else {
         if (introStage == 2) {
           document.getElementById("introText").innerHTML = "This is a game all about building you kingdom and ending others, building alliances and ending alliances, hiring and firing, and saving lives and ending them too.  ";
