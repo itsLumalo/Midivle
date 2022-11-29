20b1e8e4eaa4928843d989b4ad32aea408dd48a3
Project LuLo <67765714+project-lulo@users.noreply.github.com>
2022-11-29 09:07:49 -0500

    Update intro.js

--- js/intro.js
+++ js/intro.js
@@ -1,18 +1,22 @@
 let introStage = 0
-// unused: var introComplete = localStorage.getItem("inco?")
-// unused: var loadingClock = 0
+let loadingClock = 0
+let hasItfinishedLoading = false
 
 const introTexts = [
   "Welcome to Midivle!",
   "This is a game all about building you kingdom and ending others, building alliances and ending alliances, hiring and firing, and saving lives and ending them too.",
-  "Have fun and don't forget, only the best kingdom will rise!"
+  "Have fun and don't forget, only the best kingdom will rise!",
+  "Thanks For Playing!"
 ]
 
 function checkIntroStage() {
   if (!localStorage.inco) {
     let spaceMessage = ""
-    if (introStage === introTexts.length - 1) {
+    if (introStage == 4) {
       localStorage.inco = true
+       setTimeout(loading1, 0);
+       setTimeout(loading2, 1000);
+       setTimeout(loading3, 2000);
     } else {
       spaceMessage = " (Press Space)"
     }
@@ -25,9 +29,7 @@ checkIntroStage()
 let add = ""
 
 function loading1() {
-  // if (localStorage.inco === true) {
   document.getElementById("introText").innerHTML = "Loading." + add
-  // }
   setTimeout(loading1, 3000)
 }
 
@@ -39,26 +41,26 @@ function loading2() {
 function loading3() {
   document.getElementById("introText").innerHTML = "Loading..." + add
   setTimeout(loading3, 3000)
+  if (loadingClock == 3) {
+    finishedLoading()
+  } else {
+    loadingClock++
+    console.log(loadingClock)
+  }
 }
 
 if (localStorage.inco) {
   setTimeout(loading1, 0);
   setTimeout(loading2, 1000);
   setTimeout(loading3, 2000);
-  // Future function to load all game UI will go here
 }
 
 if (!localStorage.inco) {
   $('body').keyup(function(evt) {
     if (evt.key === " " || evt.key === "Space" || evt.keyCode === 32) {
-      // Why write this:
-      // introStage = introStage + 1;
-      // when you could use this:
-      // introStage += 1
-      // If the right side is 1, you can also use this:
       introStage++
       checkIntroStage()
-      // console.log(introStage);
+      console.log(localStorage.inco)
     }
   });
 }
@@ -67,7 +69,14 @@ function resetIncoAlert() {
   if (confirm("Only use if page is not loading!") == true) {
     delete localStorage.inco
   } else {
-    document.getElementById("cancelText").innerHTML = "Reset Was Canceled" 
+    document.getElementById("cancelText").innerHTML = "Reset Was Canceled"
     return false
-    }
+  }
+}
+
+function finishedLoading() {
+  document.getElementById("introText").hidden = true
+  document.getElementById("cancelText").hidden = true
+  document.getElementById("resetLink").hidden = true
+  hasItFinishedLoading = true
 }
